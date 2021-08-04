c5903d92d99ab6bd639b55b5b4f07ad7a4e93111
LaylaIV <lorena.rodriguez@estudiante.ceibal.edu.uy>
2021-08-04 18:45:51 -0300

    cambios clase

--- controller/lista.php
+++ controller/lista.php
@@ -10,8 +10,8 @@
 </head>
 <body>
     <?php
-include('../view/listaHead');
-include('../view/listaCont');
+include('../view/listaHead.html');
+include('../view/listaCont.html');
     ?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
