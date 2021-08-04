9409fcb1b24214b175e7b0cb719f248b26df65e4
LaylaIV <lorena.rodriguez@estudiante.ceibal.edu.uy>
2021-08-04 13:21:00 -0300

    cambios frontend

--- controller/index.php
+++ controller/index.php
@@ -5,6 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .widthImp{
+          width: 4em;
+      }
+
+      .img{
+        border-radius: 21px 21px 0 0;
+      }
+      .div__img{
+        width: 80%; height: 300px; border-radius: 21px 21px 0 0;
+      }
+      
+    </style>
     <title>Comprar</title>
 </head>
 <body>
