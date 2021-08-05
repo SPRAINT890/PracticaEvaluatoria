19e3038abe84bb035f0002e526a8105929083816
LaylaIV <lorena.rodriguez@estudiante.ceibal.edu.uy>
2021-08-05 19:20:39 -0300

    mi pass bd

--- model/conexion.php
+++ model/conexion.php
@@ -2,7 +2,7 @@
 class Conexion extends PDO{
 
  private const USER = "root";
- private const PASS = "";
+ private const PASS = "54118459";
  private const DB = "ejercicio_web";
  private const HOST = "localhost";
  private const DSN = "mysql:host=" . self::HOST . ";dbname=" . self::DB . ";charset=utf8"; 
