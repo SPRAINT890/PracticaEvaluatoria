3c654a35760a56c6e17b45078d3228ceb315f45f
LaylaIV <lorena.rodriguez@estudiante.ceibal.edu.uy>
2021-08-05 21:46:10 -0300

    prueba

--- view/pruebaLista.php
+++ view/pruebaLista.php
@@ -29,6 +29,27 @@
                 <td colspan="2">cantidad</td>
                 <td colspan="1">399</td>
             </tr>
+            <tr>
+                <th scope="row" colspan="1">1</th>
+                <td colspan="2">laura</td>
+                <td colspan="2">juaubdhj</td>
+                <td colspan="2">cantidad</td>
+                <td colspan="1">399</td>
+            </tr>
+            <tr>
+                <th scope="row" colspan="1">1</th>
+                <td colspan="2">laura</td>
+                <td colspan="2">juaubdhj</td>
+                <td colspan="2">cantidad</td>
+                <td colspan="1">399</td>
+            </tr>
+            <tr>
+                <th scope="row" colspan="1">1</th>
+                <td colspan="2">laura</td>
+                <td colspan="2">juaubdhj</td>
+                <td colspan="2">cantidad</td>
+                <td colspan="1">399</td>
+            </tr>
         </tbody>
     </table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
