57caac04ca858321eff01ab0c99ab61459137c58
Gaspar Morales <gasparmorales1@gmail.com>
2021-08-06 10:53:36 -0300

    comentario hardcodeado

--- model/conexion.php
+++ model/conexion.php
@@ -9,6 +9,9 @@ class Conexion extends PDO{
  
  public function __CONSTRUCT() {
     try{
+       /*
+       parent::__CONSTRUCT("mysql:host=localhost;dbname=ejercicio_web;charset=utf8", root, fdeQt7fkJkQEvrycdIQs);
+       */
        parent::__CONSTRUCT(self::DSN, self::USER, self::PASS);
     }catch(PDOException $e){
        echo 'Ha surgido un error y no se puede conectar a la base de datos. Detalle: ' . $e->getMessage();
